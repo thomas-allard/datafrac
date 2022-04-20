d32a1b2f4b139b0455d599caae0e01cacf89e080
swalker5 <samantha.walker-1@colorado.edu>
2022-04-19 20:50:18 -0600

    Edited py file slightly

--- abaqus/final_elastic/nc_file_ex.py
+++ abaqus/final_elastic/nc_file_ex.py
@@ -10,21 +10,26 @@ nc = netCDF4.Dataset(fname)
 print(nc)
 print(nc.variables['step_080'].time)
 print(nc.variables['step_080'][:].data)
-print(np.shape(nc.variables['step_080'][:].data))
+print(np.shape(nc.variables['step_080'][:].data)) # (2790, 9) # (nodes, fields)
+nodes_n = np.shape(nc.variables['step_080'][:].data)[0]
+fields_n = np.shape(nc.variables['step_080'][:].data)[1]
 
 # make a new array for time
 time = []
 print("Appending times to an np array")
 for ii in range(len(nc.variables)-2):
     if ii < 10:
-        print(nc.variables['step_00' + str(ii)].time)
+        #print(nc.variables['step_00' + str(ii)].time)
         time.append(nc.variables['step_00' + str(ii)].time)
     elif ii >= 10 and ii < 100:
-        print(nc.variables['step_0' + str(ii)].time)
+        #print(nc.variables['step_0' + str(ii)].time)
         time.append(nc.variables['step_0' + str(ii)].time)
     else:
-        print(nc.variables['step_' + str(ii)].time)
+        #print(nc.variables['step_' + str(ii)].time)
         time.append(nc.variables['step_' + str(ii)].time)
-time = np.asarray(time)
+time = np.asarray(time) # (81, ) (steps)
 print("Time:", time)
 print("Shape of array:", np.shape(time))
+time_n = np.shape(time)[0]
+
+test = np.empty([nodes_n, fields_n, time_n])
